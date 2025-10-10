e89615f775242a80553fa09e4ed10ac91fbb1b89
soaqa <soaqa57@gmail.com>
2025-10-10 11:07:15 +0300

    remove promo button

--- bot/keyboards/inline/user_keyboards.py
+++ bot/keyboards/inline/user_keyboards.py
@@ -34,7 +34,7 @@ def get_main_menu_inline_keyboard(
     # promo_button = InlineKeyboardButton(
     #     text=_(key="menu_apply_promo_button"),
     #     callback_data="main_action:apply_promo")
-    # builder.row(referral_button, promo_button)
+    builder.row(referral_button)
 
     language_button = InlineKeyboardButton(
         text=_(key="menu_language_settings_inline"),
