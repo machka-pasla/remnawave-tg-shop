3e6a5f663cfa840d3f7dcdeb48c87577e74d0c17
Dmity Sorokin <123333931+Sorokin97@users.noreply.github.com>
2025-11-08 15:32:32 +0300

    Consolidate start welcome message and refresh menu

--- bot/handlers/user/start.py
+++ bot/handlers/user/start.py
@@ -1,7 +1,6 @@
 import logging
 import re
 from aiogram import Router, F, types, Bot
-from aiogram.utils.text_decorations import html_decoration as hd
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.context import FSMContext
 from typing import Optional, Union
@@ -39,7 +38,6 @@ async def send_main_menu(target_event: Union[types.Message,
     i18n: Optional[JsonI18n] = i18n_data.get("i18n_instance")
 
     user_id = target_event.from_user.id
-    user_full_name = hd.quote(target_event.from_user.full_name)
 
     if not i18n:
         logging.error(
@@ -73,7 +71,12 @@ async def send_main_menu(target_event: Union[types.Message,
                 "Method has_had_any_subscription is missing in SubscriptionService for send_main_menu!"
             )
 
-    text = _(key="main_menu_greeting", user_name=user_full_name)
+    text = (
+        "🌐 <b>VPN•PRO</b> надёжный помощник в мире безграничного интернета!\n\n"
+        "📡 Приватное и быстрое <b>VPN-подключение</b> без лишних сложностей.\n\n"
+        "💬 <a href=\"https://t.me/rusys\">Служба поддержки</a>\n\n"
+        "Панель управления ⤵️"
+    )
     reply_markup = get_main_menu_inline_keyboard(current_lang, i18n, settings,
                                                  show_trial_button_in_menu)
 
@@ -95,9 +98,17 @@ async def send_main_menu(target_event: Union[types.Message,
 
     try:
         if is_edit:
-            await target_message_obj.edit_text(text, reply_markup=reply_markup)
+            await target_message_obj.edit_text(
+                text,
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+            )
         else:
-            await target_message_obj.answer(text, reply_markup=reply_markup)
+            await target_message_obj.answer(
+                text,
+                reply_markup=reply_markup,
+                disable_web_page_preview=True,
+            )
 
         if isinstance(target_event, types.CallbackQuery):
             try:
@@ -110,7 +121,11 @@ async def send_main_menu(target_event: Union[types.Message,
         )
         if is_edit and target_message_obj:
             try:
-                await target_message_obj.answer(text, reply_markup=reply_markup)
+                await target_message_obj.answer(
+                    text,
+                    reply_markup=reply_markup,
+                    disable_web_page_preview=True,
+                )
             except Exception as e_send_new:
                 logging.error(
                     f"Also failed to send new main menu message for user {user_id}: {e_send_new}"
@@ -433,10 +448,6 @@ async def start_command_handler(message: types.Message,
                                                       db_user):
         return
 
-    # Send welcome message if not disabled
-    if not settings.DISABLE_WELCOME_MESSAGE:
-        await message.answer(_(key="welcome", user_name=hd.quote(user.full_name)))
-
     # Auto-apply promo code if provided via start parameter
     if promo_code_to_apply:
         try:
@@ -515,17 +526,6 @@ async def verify_channel_subscription_callback(
         _ = lambda key, **kwargs: i18n.gettext(current_lang, key, **kwargs)
     else:
         _ = lambda key, **kwargs: key
-
-    if not settings.DISABLE_WELCOME_MESSAGE:
-        welcome_text = _(key="welcome",
-                         user_name=hd.quote(callback.from_user.full_name))
-        if callback.message:
-            await callback.message.answer(welcome_text)
-        else:
-            fallback_bot: Optional[Bot] = getattr(callback, "bot", None)
-            if fallback_bot:
-                await fallback_bot.send_message(callback.from_user.id,
-                                                welcome_text)
 
     try:
         await callback.answer(_(key="channel_subscription_verified_success"),
@@ -661,8 +661,16 @@ async def main_action_callback_handler(
     elif action == "request_trial":
         await user_trial_handlers.request_trial_confirmation_handler(
             callback, settings, i18n_data, subscription_service, session)
+    elif action == "instructions":
+        i18n: Optional[JsonI18n] = i18n_data.get("i18n_instance")
+        current_lang = i18n_data.get("current_language", settings.DEFAULT_LANGUAGE)
+        if i18n:
+            placeholder_text = i18n.gettext(
+                current_lang, "menu_instructions_placeholder")
+        else:
+            placeholder_text = "Instructions are not available yet."
+        await callback.answer(placeholder_text, show_alert=True)
     elif action == "language":
-
         await language_command_handler(callback, i18n_data, settings)
     elif action == "back_to_main":
         await send_main_menu(callback,
